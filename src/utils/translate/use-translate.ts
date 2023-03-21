7cea3b4a3692eef8e1bc9f0a4657d230b865d06a
korsakov <korsakov.ua.dn@gmail.com>
2023-03-21 15:25:26 +0300

    check * import for all locales at hosting

--- src/utils/translate/use-translate.ts
+++ src/utils/translate/use-translate.ts
@@ -1,19 +1,21 @@
-import en from './locales/en.json';
-import ru from './locales/ru.json';
+// import en from './locales/en.json';
+// import ru from './locales/ru.json';
+import * as locales from './locales';
 
 export function useTranslation<T extends keyof Wordbook>(
   chapter: T,
   locale: Locale
 ) {
   // const wordbook: Wordbook = require(`./locales/${locale}.json`);
+  // eslint-disable-next-line import/namespace
+  const wordbook: Wordbook = locales[locale];
 
-  let wordbook: Wordbook;
-
-  if (locale === 'en') {
-    wordbook = en;
-  } else {
-    wordbook = ru;
-  }
+  // let wordbook: Wordbook;
+  // if (locale === 'en') {
+  //   wordbook = en;
+  // } else {
+  //   wordbook = ru;
+  // }
 
   return (key: keyof Wordbook[T]) => wordbook[chapter][key];
 }
@@ -21,6 +23,9 @@ export function useTranslation<T extends keyof Wordbook>(
 // types
 export type Locale = 'en' | 'ru';
 
-type Wordbook = typeof ru | typeof en;
+// type Wordbook = typeof ru | typeof en;
 
 export type Translate = ReturnType<typeof useTranslation>;
+
+type ValueOf<T> = T[keyof T];
+type Wordbook = ValueOf<typeof locales>;
